7320d241f2b3f8a9cd559520636ffff79290717c
hello-botcmd <swaruppanda2008@gmail.com>
2026-08-17 23:19:25 +0530

    Create state.py

---
+++ bot/utils /state.py
@@ -0,0 +1,23 @@
+class UserState:
+    """Tracks what each user is currently doing."""
+    def __init__(self):
+        self.hex_wait = {}     # user_id -> "manage" | "guard"
+        self.active_hex = {}   # user_id -> hex currently loaded in Manage flow
+
+    def set_wait(self, uid, mode):
+        self.hex_wait[uid] = mode
+
+    def waiting(self, uid):
+        return self.hex_wait.get(uid)
+
+    def clear_wait(self, uid):
+        self.hex_wait.pop(uid, None)
+
+    def set_hex(self, uid, hex_str):
+        self.active_hex[uid] = hex_str
+
+    def get_hex(self, uid):
+        return self.active_hex.get(uid)
+
+    def clear_hex(self, uid):
+        self.active_hex.pop(uid, None)
