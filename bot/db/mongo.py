114c50400e2fbb5b511c061c1d3acfbc38c06e92
hello-botcmd <swaruppanda2008@gmail.com>
2026-08-19 01:52:06 +0530

    Increase MongoDB server selection timeout to 5000ms

--- bot/db/mongo.py
+++ bot/db/mongo.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
+
 from config import MONGO_URI, DB_NAME
 
-_client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=3000)
+_client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
+
 
 def get_db():
     return _client[DB_NAME]
